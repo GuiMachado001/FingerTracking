e84660373756c754ac37ba865aea6124371efad0
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2024-07-08 11:40:40 -0400

    .

--- testes.py
+++ testes.py
@@ -1,91 +1,79 @@
 import cv2
 import mediapipe as mp
+import numpy as np
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
 
+# For static images:
+IMAGE_FILES = []
+BG_COLOR = (192, 192, 192) # gray
+with mp_pose.Pose(
+    static_image_mode=True,
+    model_complexity=2,
+    enable_segmentation=True,
+    min_detection_confidence=0.5) as pose:
+  for idx, file in enumerate(IMAGE_FILES):
+    image = cv2.imread(file)
+    image_height, image_width, _ = image.shape
+    # Convert the BGR image to RGB before processing.
+    results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-##captura de video
-video = cv2.VideoCapture(0)
+    if not results.pose_landmarks:
+      continue
+    print(
+        f'Nose coordinates: ('
+        f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width}, '
+        f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_height})'
+    )
 
-#Detectar mãos
-hand = mp.solutions.hands
-Hand = hand.Hands(max_num_hands=1)
+    annotated_image = image.copy()
+    # Draw segmentation on the image.
+    # To improve segmentation around boundaries, consider applying a joint
+    # bilateral filter to "results.segmentation_mask" with "image".
+    condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
+    bg_image = np.zeros(image.shape, dtype=np.uint8)
+    bg_image[:] = BG_COLOR
+    annotated_image = np.where(condition, annotated_image, bg_image)
+    # Draw pose landmarks on the image.
+    mp_drawing.draw_landmarks(
+        annotated_image,
+        results.pose_landmarks,
+        mp_pose.POSE_CONNECTIONS,
+        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+    cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
+    # Plot pose world landmarks.
+    mp_drawing.plot_landmarks(
+        results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
 
-#Desenhar mãos
-mpDraw = mp.solutions.drawing_utils
+# For webcam input:
+cap = cv2.VideoCapture(0)
+with mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as pose:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
 
-##capturar video
-while True:
-    check,img = video.read()
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = pose.process(image)
 
-    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-
-    results = Hand.process(imgRGB)
-
-    handsPoints = results.multi_hand_landmarks
-
-    h,w,_= img.shape
-
-    pontos = []
-
-    if handsPoints:
-        for points in handsPoints:
-            #print(points)
-            mpDraw.draw_landmarks(img, points, hand.HAND_CONNECTIONS)
-
-            for id,cord in enumerate(points.landmark):
-                cx,cy = int(cord.x*w),int(cord.y*h)
-                #cv2.putText(img,str(id),(cx,cy+10),cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2)
-                pontos.append((cx,cy))
-
-        dedos = [8,12,16,20]
-
-        gesto = ""
-
-        # --------------------- A L F A B E T O -----------------------------
-
-        ## -------------DEDÃO------------------------INDICADOR----------------------------MEIO---------------------------ANELAR---------------------------MINDINHO------------
-        if pontos[4][0] > pontos[2][0] and pontos[8][1] > pontos[5][1] and pontos[12][1] > pontos[9][1] and pontos[16][1] > pontos[13][1] and pontos[20][1] > pontos[17][1]:
-            gesto += "A"
-
-        ## -------------DEDÃO------------------------INDICADOR----------------------------MEIO---------------------------ANELAR---------------------------MINDINHO------------
-        elif pontos[4][0] < pontos[2][0] and pontos[8][1] < pontos[5][1] and pontos[12][1] < pontos[19][1] and pontos[16][1] < pontos[13][1] and pontos[20][1] < pontos[19][1]:
-            gesto += "B"
-
-        ## -------------DEDÃO------------------------INDICADOR----------------------------MEIO---------------------------ANELAR---------------------------MINDINHO------------
-        elif pontos[4][0] > pontos[2][0] and pontos[8][1] > pontos[6][1] and pontos[12][1] > pontos[10][1] and pontos[16][1] > pontos[14][1] and pontos[20][1] > pontos[19][1]:
-            gesto += "C"
-
-        ## -------------DEDÃO------------------------INDICADOR----------------------------MEIO---------------------------ANELAR---------------------------MINDINHO------------
-        elif pontos[4][0] < pontos[2][0] and pontos[8][1] < pontos[6][1] and pontos[12][1] > pontos[10][1] and pontos[16][1] > pontos[14][1] and pontos[20][1] > pontos[19][1]:
-            gesto += "D"
-
-        ## -------------DEDÃO------------------------INDICADOR----------------------------MEIO---------------------------ANELAR---------------------------MINDINHO------------
-        elif pontos[4][0] < pontos[2][0] and pontos[8][1] > pontos[5][1] and pontos[12][1] > pontos[9][1] and pontos[16][1] > pontos[13][1] and pontos[20][1] > pontos[17][1]:
-            gesto += "E"
-
-        ## -------------DEDÃO------------------------INDICADOR----------------------------MEIO---------------------------ANELAR---------------------------MINDINHO------------
-        elif pontos[4][0] < pontos[2][0] and pontos[8][1] > pontos[7][1] and pontos[12][1] < pontos[10][1] and pontos[16][1] < pontos[14][1] and pontos[20][1] < pontos[19][1]:
-            gesto += "F"
-
-        ## -------------DEDÃO------------------------INDICADOR----------------------------MEIO---------------------------ANELAR---------------------------MINDINHO------------
-        elif pontos[4][0] > pontos[2][0] and pontos[8][1] < pontos[5][1] and pontos[12][1] > pontos[9][1] and pontos[16][1] > pontos[13][1] and pontos[20][1] > pontos[17][1]:
-            gesto += "G"
-
-
-        ## -------------DEDÃO------------------------INDICADOR----------------------------MEIO---------------------------ANELAR---------------------------MINDINHO------------
-        elif pontos[4][0] > pontos[2][0] and pontos[8][1] < pontos[6][1] and pontos[12][1] < pontos[10][1] and pontos[16][1] > pontos[14][1] and pontos[20][1] > pontos[19][1]:
-            gesto += "H"
-
-        ## -------------DEDÃO------------------------INDICADOR----------------------------MEIO---------------------------ANELAR---------------------------MINDINHO------------
-        elif pontos[4][0] < pontos[2][0] and pontos[8][1] > pontos[5][1] and pontos[12][1] > pontos[9][1] and pontos[16][1] > pontos[13][1] and pontos[20][1] < pontos[17][1]:
-            gesto += "i"
-
-
-
-        cv2.putText(img, gesto, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 4, (255, 0, 0), 15)
-        cv2.imshow("Imagem", img)
-
-    cv2.imshow("Imagem", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-video.release()
-cv2.destroyAllWindows()
+    # Draw the pose annotation on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    mp_drawing.draw_landmarks(
+        image,
+        results.pose_landmarks,
+        mp_pose.POSE_CONNECTIONS,
+        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+    # Flip the image horizontally for a selfie-view display.
+    cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+cap.release()
